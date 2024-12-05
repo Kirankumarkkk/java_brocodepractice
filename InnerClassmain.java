f8693895f5ea664e4aa40fcfd8c8bcf3027bdb7d
Kirankumarkkk <87245413+Kirankumarkkk@users.noreply.github.com>
2024-12-05 13:46:31 +0530

    Add files via upload

--- InnerClassmain.java
+++ InnerClassmain.java
@@ -1,8 +1,32 @@
 public class InnerClassmain {
     public static void main(String[] args) {
 
+        //outSideclass and inSideclass both classes
         outSideclass out=new outSideclass();
         outSideclass.inSideclass in=out.new inSideclass();
         in.greetings();
+
+        //innerclassGreeting override 1
+
+        innerclassGreeting innerclassGreeting1=new innerclassGreeting(){
+            @Override
+            public void greetings() {
+                System.out.println("yo bro");
+            }
+        };
+
+        innerclassGreeting1.greetings();
+
+        //innerclassGreeting override 2
+
+        innerclassGreeting innerclassGreeting2=new innerclassGreeting(){
+            @Override
+            public void greetings() {
+                System.out.println("yo hello");
+            }
+        };
+
+        innerclassGreeting2.greetings();
+
     }
 }
---
+++ innerclassGreeting.java
@@ -0,0 +1,5 @@
+public class innerclassGreeting {
+    public void greetings(){
+        System.out.println("Hello greetings");
+    }
+}
