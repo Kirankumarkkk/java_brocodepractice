4ae03a6bee524c66a16163d19591cc2e94bcf41b
Kirankumarkkk <87245413+Kirankumarkkk@users.noreply.github.com>
2024-12-05 13:02:08 +0530

    Add files via upload

---
+++ customExceptionmain.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class customExceptionmain {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter your age : ");
+        int age=scan.nextInt();
+
+        try {
+            checkAge(age);
+        } catch (Exception e)
+        {
+            System.out.println("A problem occured at "+ e);
+        }
+    }
+
+    static void checkAge(int age)throws AgeException{
+        if(age<18) {
+            throw new AgeException("\n"+"You must be 18+ to sign up");
+        }
+        else {
+            System.out.println("You are now signed up!");
+        }
+    }
+}
